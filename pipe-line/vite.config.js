9703d25d2621936644f5f0001e71a28a33111a50
theirhasnain <theirhasnain@gmail.com>
2026-06-23 10:09:09 -0700

    edited code

--- pipe-line/vite.config.js
+++ pipe-line/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // Use `npm run build:ghpages` when deploying to GitHub Pages project sites.
+  base: mode === 'ghpages' ? '/kiwi-logics-pipe-line/' : '/',
   plugins: [react()],
-})
+}))
