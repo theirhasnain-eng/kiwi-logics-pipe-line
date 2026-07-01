38ccafae6bbb6444b3b4287312037a70709b7b65
theirhasnain <theirhasnain@gmail.com>
2026-07-01 00:07:22 -0700

    artical page is edited

--- pipe-line/src/pages/Article.jsx
+++ pipe-line/src/pages/Article.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { blogs } from "../data/BlogData";
 import "./styling/artical.css";
+import { FaXTwitter, FaFacebookF, FaLinkedinIn, FaLink } from "react-icons/fa6";
 
 export default function Article() {
   const { slug } = useParams();
@@ -10,9 +11,7 @@ export default function Article() {
     window.scrollTo(0, 0);
   }, [slug]);
 
-  const article = blogs.find(
-    (blog) => blog.slug === slug
-  );
+  const article = blogs.find((blog) => blog.slug === slug);
 
   if (!article) {
     return <h1>Article Not Found</h1>;
@@ -20,15 +19,13 @@ export default function Article() {
 
   const relatedBlogs = blogs.filter(
     (blog) =>
-      blog.category.toLowerCase() ===
-        article.category.toLowerCase() &&
-      blog.id !== article.id
+      blog.category.toLowerCase() === article.category.toLowerCase() &&
+      blog.id !== article.id,
   );
 
   return (
     <section className="article-page">
       <div className="article-container">
-
         <Link to="/blog" className="back-btn">
           ← Back to Blog
         </Link>
@@ -41,32 +38,20 @@ export default function Article() {
           <Link to="/blog" className="blog">
             Blog
           </Link>
-          /
-          <span>Article</span>
+          /<span>Article</span>
         </div>
 
-        <span className="article-category">
-          {article.category}
-        </span>
+        <span className="article-category">{article.category}</span>
 
-        <h1 className="article-title">
-          {article.title}
-        </h1>
+        <h1 className="article-title">{article.title}</h1>
 
-        <p className="article-description">
-          {article.description}
-        </p>
+        <p className="article-description">{article.description}</p>
 
         <div className="article-author">
-          <img
-            src={article.imag}
-            alt={article.author}
-          />
+          <img src={article.imag} alt={article.author} />
 
           <div className="author-info">
-            <span className="author-name">
-              {article.author}
-            </span>
+            <span className="author-name">{article.author}</span>
 
             <span className="author-meta">
               {article.date} • {article.readTime}
@@ -83,16 +68,42 @@ export default function Article() {
         />
 
         <div className="article-content">
-          {article.content
-            .split("\n")
-            .filter((paragraph) => paragraph.trim() !== "")
-            .map((paragraph, index) => (
-              <p key={index}>
-                {paragraph}
+          {article.content.map((section, index) =>
+            section.type === "heading" ? (
+              <h3 key={index} className="content-heading">
+                {section.text}
+              </h3>
+            ) : (
+              <p key={index} className="content-paragraph">
+                {section.text}
               </p>
-            ))}
+            ),
+          )}
         </div>
-       {/* <div className="article-divider"></div> */}
+        <div className="article-divider"></div>
+        <div className="share-article">
+          <h3>Share this article</h3>
+
+          <div className="share-icons">
+            <a href="#" className="share-icon" aria-label="Share on X">
+              <FaXTwitter />
+            </a>
+
+            <a href="#" className="share-icon" aria-label="Share on Facebook">
+              <FaFacebookF />
+            </a>
+
+            <a href="#" className="share-icon" aria-label="Share on LinkedIn">
+              <FaLinkedinIn />
+            </a>
+
+            <button className="share-icon" aria-label="Copy Link">
+              <FaLink />
+            </button>
+          </div>
+        </div>
+      </div>
+      <section className="related-artical">
         {relatedBlogs.length > 0 && (
           <section className="related-section">
             <h2>Related Articles</h2>
@@ -105,10 +116,7 @@ export default function Article() {
                   className="related-card"
                 >
                   <div className="related-image">
-                    <img
-                      src={blog.image}
-                      alt={blog.title}
-                    />
+                    <img src={blog.image} alt={blog.title} />
                   </div>
 
                   <div className="related-content">
@@ -123,8 +131,7 @@ export default function Article() {
             </div>
           </section>
         )}
-
-      </div>
+      </section>
     </section>
   );
 }
